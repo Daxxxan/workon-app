5384af09d74d7509354c2c9fa317e3e25b4db436
Daxxxan <alex-bad97@hotmail.fr>
2018-06-17 17:09:03 +0200

    Error label project name

--- src/main/java/com/workon/controllers/ProjectController.java
+++ src/main/java/com/workon/controllers/ProjectController.java
@@ -9,12 +9,15 @@ import com.workon.models.User;
 import com.workon.plugin.CoffeePluginInterface;
 import com.workon.utils.ButtonHelper;
 import com.workon.utils.HttpRequest;
+import com.workon.utils.LabelHelper;
 import com.workon.utils.ParseRequestContent;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
@@ -50,6 +53,9 @@ public class ProjectController {
 
     @FXML
     private JFXButton createDocumentationButton;
+
+    @FXML
+    private Label projectNameErrorLabel;
 
     private ArrayList<JFXTextField> textFieldCollaboratorArray = new ArrayList<>();
     private ArrayList<JFXTextField> textFieldStepNameArray = new ArrayList<>();
@@ -122,115 +128,120 @@ public class ProjectController {
 
     @FXML
     protected void handleValidateProjectButtonAction() throws Exception {
-        Project project = new Project();
-        //Root pour la création d'un projet
-        String createProjectRequest = "http://localhost:3000/api/accounts/".concat(Integer.toString(LoginConnectionController.getUserId())).concat("/projects");
-        Map<String, String> createProjectParameters = new HashMap<>();
-        createProjectParameters.put("name", projectNameTextField.getText());
+        if(!(projectNameTextField.getText().isEmpty())){
+            Project project = new Project();
+            //Root pour la création d'un projet
+            String createProjectRequest = "http://localhost:3000/api/accounts/".concat(Integer.toString(LoginConnectionController.getUserId())).concat("/projects");
+            Map<String, String> createProjectParameters = new HashMap<>();
+            createProjectParameters.put("name", projectNameTextField.getText());
 
-        //Lancement de la création d'un projet
-        StringBuffer contentCreateProject = HttpRequest.setRequest(createProjectRequest, createProjectParameters, null, "POST", null, LoginConnectionController.getUserToken());
+            //Lancement de la création d'un projet
+            StringBuffer contentCreateProject = HttpRequest.setRequest(createProjectRequest, createProjectParameters, null, "POST", null, LoginConnectionController.getUserToken());
 
-        //Si le projet a été créé
-        if(contentCreateProject != null){
-            //Récupération de l'ID du projet créé
-            String projectId = ParseRequestContent.getValueOf(contentCreateProject, "\"id\"");
-            projectId = projectId.substring(0, Objects.requireNonNull(projectId).length() - 1);
-            String addCollaboratorsRequest;
-            String addStepsRequest;
+            //Si le projet a été créé
+            if(contentCreateProject != null){
+                //Récupération de l'ID du projet créé
+                String projectId = ParseRequestContent.getValueOf(contentCreateProject, "\"id\"");
+                projectId = projectId.substring(0, Objects.requireNonNull(projectId).length() - 1);
+                String addCollaboratorsRequest;
+                String addStepsRequest;
 
-            //Set d'un objet ProjectController
-            project.setName(projectNameTextField.getText());
-            project.setDirector(LoginConnectionController.getUserId());
+                //Set d'un objet ProjectController
+                project.setName(projectNameTextField.getText());
+                project.setDirector(LoginConnectionController.getUserId());
 
-            //Root pour l'ajout du directeur aux collaborateurs
-            String addDirectorRequest = "http://localhost:3000/api/accounts/".concat(Integer.toString(LoginConnectionController.getUserId()))
-                    .concat("/projects/").concat(projectId).concat("/accounts/rel/").concat(Integer.toString(LoginConnectionController.getUserId()));
+                //Root pour l'ajout du directeur aux collaborateurs
+                String addDirectorRequest = "http://localhost:3000/api/accounts/".concat(Integer.toString(LoginConnectionController.getUserId()))
+                        .concat("/projects/").concat(projectId).concat("/accounts/rel/").concat(Integer.toString(LoginConnectionController.getUserId()));
 
-            //Lancement de l'ajout du directeur aux collaborateurs
-            StringBuffer contentAddDirectorToProjectCollaborators = HttpRequest.setRequest(addDirectorRequest, null, null, "PUT", null, LoginConnectionController.getUserToken());
+                //Lancement de l'ajout du directeur aux collaborateurs
+                StringBuffer contentAddDirectorToProjectCollaborators = HttpRequest.setRequest(addDirectorRequest, null, null, "PUT", null, LoginConnectionController.getUserToken());
 
-            //Si le directeur a bien été ajouté
-            if(contentAddDirectorToProjectCollaborators != null){
-                ArrayList<User> usersList = new ArrayList<>();
-                User director = new User(LoginConnectionController.getUserId());
-                director.setRole("Director");
-                usersList.add(director);
+                //Si le directeur a bien été ajouté
+                if(contentAddDirectorToProjectCollaborators != null){
+                    ArrayList<User> usersList = new ArrayList<>();
+                    User director = new User(LoginConnectionController.getUserId());
+                    director.setRole("Director");
+                    usersList.add(director);
 
-                //Ajout de chaque collaborateurs au projet
-                for(JFXTextField textField : getTextFieldCollaboratorArray()) {
-                    //Requête pour l'ajout des collaborateurs au projet
-                    addCollaboratorsRequest = "http://localhost:3000/api/accounts/".concat(Integer.toString(LoginConnectionController.getUserId()))
-                            .concat("/projects/").concat(projectId).concat("/accounts/rel/").concat(textField.getText());
-                    //Lancement de l'ajout des collaborateurs
-                    StringBuffer contentAddCollaboratorsToProject = HttpRequest.setRequest(addCollaboratorsRequest, null, null, "PUT", null, LoginConnectionController.getUserToken());
-                    if(contentAddCollaboratorsToProject != null){
-                        //Récupération de l'id du collaborateur
-                        int collaboratorId = Integer.parseInt(Objects.requireNonNull(ParseRequestContent.getValueOf(contentAddCollaboratorsToProject, "\"accountId\"")));
+                    //Ajout de chaque collaborateurs au projet
+                    for(JFXTextField textField : getTextFieldCollaboratorArray()) {
+                        //Requête pour l'ajout des collaborateurs au projet
+                        addCollaboratorsRequest = "http://localhost:3000/api/accounts/".concat(Integer.toString(LoginConnectionController.getUserId()))
+                                .concat("/projects/").concat(projectId).concat("/accounts/rel/").concat(textField.getText());
+                        //Lancement de l'ajout des collaborateurs
+                        StringBuffer contentAddCollaboratorsToProject = HttpRequest.setRequest(addCollaboratorsRequest, null, null, "PUT", null, LoginConnectionController.getUserToken());
+                        if(contentAddCollaboratorsToProject != null){
+                            //Récupération de l'id du collaborateur
+                            int collaboratorId = Integer.parseInt(Objects.requireNonNull(ParseRequestContent.getValueOf(contentAddCollaboratorsToProject, "\"accountId\"")));
 
-                        //Vérifier que l'id n'est pas déjà dans le tableau des users
-                        int exist = 0;
-                        for (User anUser : usersList) {
-                            if (collaboratorId == anUser.getId()) {
-                                exist = 1;
+                            //Vérifier que l'id n'est pas déjà dans le tableau des users
+                            int exist = 0;
+                            for (User anUser : usersList) {
+                                if (collaboratorId == anUser.getId()) {
+                                    exist = 1;
+                                }
+                            }
+                            //Si le user n'existe pas on le créer
+                            if(exist == 0){
+                                User user = new User(collaboratorId, textField.getText());
+                                usersList.add(user);
                             }
                         }
-                        //Si le user n'existe pas on le créer
-                        if(exist == 0){
-                            User user = new User(collaboratorId, textField.getText());
-                            usersList.add(user);
+                    }
+                    //Ajout du tableau d'utilisateur au projet local
+                    project.setUsers(usersList);
+
+                    //Ajout de chaque steps au projet
+                    ArrayList<Step> stepsList = new ArrayList<>();
+                    for(int counter = 0; counter < getTextFieldStepNameArray().size(); counter++){
+                        //Request pour l'ajout des steps au projet
+                        addStepsRequest = "http://localhost:3000/api/accounts/".concat(Integer.toString(LoginConnectionController.getUserId()))
+                                .concat("/projects/").concat(projectId).concat("/steps");
+
+                        //Convert LocalDate to String
+                        DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
+                        String stepDate = getDatePickerStepArray().get(counter).getValue().format(formatter);
+
+                        //Set des parameters de la requete en POST
+                        Map<String, String> parameters = new HashMap<>();
+                        parameters.put("name", getTextFieldStepNameArray().get(counter).getText());
+                        parameters.put("date", stepDate);
+                        parameters.put("state", "En cours");
+
+                        //Lancement de l'ajout des steps
+                        StringBuffer contentAddStepsToProject = HttpRequest.setRequest(addStepsRequest, parameters, null, "POST", null, LoginConnectionController.getUserToken());
+
+                        //Si l'ajout a bien été effectué
+                        if(contentAddStepsToProject != null){
+                            Step step = new Step(getTextFieldStepNameArray().get(counter).getText(), getDatePickerStepArray().get(counter).getValue());
+                            stepsList.add(step);
                         }
                     }
-                }
-                //Ajout du tableau d'utilisateur au projet local
-                project.setUsers(usersList);
+                    //Ajout des steps au projet local
+                    project.setSteps(stepsList);
+                    //Création du projet global
+                    setProject(project);
 
-                //Ajout de chaque steps au projet
-                ArrayList<Step> stepsList = new ArrayList<>();
-                for(int counter = 0; counter < getTextFieldStepNameArray().size(); counter++){
-                    //Request pour l'ajout des steps au projet
-                    addStepsRequest = "http://localhost:3000/api/accounts/".concat(Integer.toString(LoginConnectionController.getUserId()))
-                    .concat("/projects/").concat(projectId).concat("/steps");
+                    //Création du bouton projet dans l'interface
+                    JFXButton button = ButtonHelper.setButton(projectNameTextField.getText(), projectId, Double.MAX_VALUE,
+                            "-fx-border-color: #000000");
 
-                    //Convert LocalDate to String
-                    DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE;
-                    String stepDate = getDatePickerStepArray().get(counter).getValue().format(formatter);
-
-                    //Set des parameters de la requete en POST
-                    Map<String, String> parameters = new HashMap<>();
-                    parameters.put("name", getTextFieldStepNameArray().get(counter).getText());
-                    parameters.put("date", stepDate);
-                    parameters.put("state", "En cours");
-
-                    //Lancement de l'ajout des steps
-                    StringBuffer contentAddStepsToProject = HttpRequest.setRequest(addStepsRequest, parameters, null, "POST", null, LoginConnectionController.getUserToken());
-
-                    //Si l'ajout a bien été effectué
-                    if(contentAddStepsToProject != null){
-                        Step step = new Step(getTextFieldStepNameArray().get(counter).getText(), getDatePickerStepArray().get(counter).getValue());
-                        stepsList.add(step);
-                    }
-                }
-                //Ajout des steps au projet local
-                project.setSteps(stepsList);
-                //Création du projet global
-                setProject(project);
-
-                //Création du bouton projet dans l'interface
-                JFXButton button = ButtonHelper.setButton(projectNameTextField.getText(), projectId, Double.MAX_VALUE,
-                        "-fx-border-color: #000000");
-
-                //Récupération de la VBox pour insérer le bouton
-                ObservableList<Node> observableList = validateProjectButton.getParent().getParent().getParent().getParent()
-                        .getParent().getChildrenUnmodifiable();
-                for(Node node : observableList){
-                    if(Objects.equals(node.getId(), "scrollPaneProjectList")){
-                        ScrollPane scrollPane = (ScrollPane)node;
-                        VBox projectListVBox = (VBox)scrollPane.lookup("#projectListVBox");
-                        projectListVBox.getChildren().add(button);
+                    //Récupération de la VBox pour insérer le bouton
+                    ObservableList<Node> observableList = validateProjectButton.getParent().getParent().getParent().getParent()
+                            .getParent().getChildrenUnmodifiable();
+                    for(Node node : observableList){
+                        if(Objects.equals(node.getId(), "scrollPaneProjectList")){
+                            ScrollPane scrollPane = (ScrollPane)node;
+                            VBox projectListVBox = (VBox)scrollPane.lookup("#projectListVBox");
+                            projectListVBox.getChildren().add(button);
+                        }
                     }
                 }
             }
+        }else{
+            System.out.println("ok");
+            LabelHelper.setLabel(projectNameErrorLabel, "Veuillez saisir le nom du projet", Pos.CENTER_LEFT, "#FF0000");
         }
     }
 
